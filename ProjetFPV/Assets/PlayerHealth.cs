30508e004f7d78a5d46f996b7993d39bbb637377
Skorthy <t.mottet@rubika-edu.com>
2024-03-16 17:29:34 +0100

    Vignette dégâts subis

--- ProjetFPV/Assets/PlayerHealth.cs
+++ ProjetFPV/Assets/PlayerHealth.cs
@@ -1,3 +1,5 @@
+using System;
+using NaughtyAttributes;
 using UnityEngine;
 using UnityEngine.Rendering;
 
@@ -12,6 +14,9 @@ public class PlayerHealth : MonoBehaviour
     
     [Tooltip("PV régénérés chaque seconde")] 
     [SerializeField] private float regenPerSecond; //Nombre de PV soignés chaque seconde
+    
+    [Tooltip("Intensité de la vignette selon les PV perdus")] 
+    [SerializeField] private AnimationCurve vignetteIntensity; //Intensité de la vignette
 
     [SerializeField] private Volume volume;
     
@@ -36,8 +41,10 @@ public class PlayerHealth : MonoBehaviour
     void Update()
     {
         t -= Time.deltaTime;
-        
-        //volume.weight = Math.Max((maxHealth - currentHealth) / maxHealth, 1f); //Set l'intensité du global volume selon les dégâts subis (max 1f)
+
+        var lostHealth = (maxHealth - currentHealth) / maxHealth;
+
+        volume.weight = Mathf.Lerp(volume.weight, vignetteIntensity.Evaluate(lostHealth), .01f);
         
         if (t <= 0 && currentHealth < maxHealth) //La régén ne s'applique qu'après que timeBeforeRegen soit passé
         {
