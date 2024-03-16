b891ddd0177ca77c53b782dc7e1ac5478d1cc639
anaturu <a.marotine@rubika-edu.com>
2024-03-16 18:14:01 +0100

    small adjustments

--- ProjetFPV/Assets/GymBullet.cs
+++ ProjetFPV/Assets/GymBullet.cs
@@ -2,11 +2,12 @@ using UnityEngine;
 
 public class GymBullet : MonoBehaviour
 {
+    public int bulletDamage;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            other.gameObject.GetComponent<PlayerHealth>().TakeDamage(15);
+            other.gameObject.GetComponent<PlayerHealth>().TakeDamage(bulletDamage);
             Destroy(gameObject);
         }
     }
