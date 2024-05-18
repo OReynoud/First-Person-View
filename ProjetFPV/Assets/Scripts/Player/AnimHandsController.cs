5b6f15db00ce9393d9d1e14a51e41f444f936860
Oscar <o.reynoud@rubika-edu.com>
2024-05-18 22:20:48 +0200

    Début anims

---
+++ ProjetFPV/Assets/Scripts/Player/AnimHandsController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimHandsController : MonoBehaviour
+{
+    private AnimationCurve animationState;
+    
+    //LEFT HAND ANIMS
+    //GRAB = 0.20 - 1.18
+    //RELEASE = 2.03 - 2.27
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
